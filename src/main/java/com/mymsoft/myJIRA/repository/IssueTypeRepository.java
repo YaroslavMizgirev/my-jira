f874ebf8fcdfb63ba383dd530de20627b56013c2
mym <yaroslav@mizgirev.ru>
2025-11-09 00:09:14 +0300

    Update java pakage

--- src/main/java/com/mymsoft/myJIRA/repository/IssueTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mymsoft.myJIRA.repository;
-
-import com.mymsoft.myJIRA.model.IssueType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IssueTypeRepository extends JpaRepository<IssueType, Long> {
-}
