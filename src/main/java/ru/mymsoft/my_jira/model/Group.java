4b84c925a019c28f92bf4d190243b56ac6e433bc
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-13 22:00:24 +0300

    Update Group

---
+++ src/main/java/ru/mymsoft/my_jira/model/Group.java
@@ -0,0 +1,38 @@
+package ru.mymsoft.my_jira.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.*;
+
+@Entity
+@Table(name = "groups",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_groups_name", columnNames = {"name"}),
+    })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of ={"id", "name", "isSystemGroup"})
+@Builder
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "is_system_group", nullable = false)
+    private Boolean isSystemGroup = false;
+}
