ef4364d79271c58a02b9d103ad8f0c217c37bd67
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-13 22:01:33 +0300

    Update IssueLinkType

---
+++ src/main/java/ru/mymsoft/my_jira/model/IssueLinkType.java
@@ -0,0 +1,28 @@
+package ru.mymsoft.my_jira.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "issue_link_types",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_issue_link_types_name", columnNames = {"name"}),
+        @UniqueConstraint(name = "uk_issue_link_types_inward_name", columnNames = {"inward_name"})
+    })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class IssueLinkType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "inward_name", nullable = false, length = 100)
+    private String inwardName;
+}
