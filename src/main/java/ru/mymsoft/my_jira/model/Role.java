edc182b8ab5b3878524aa03917bb5f4e3ecf2d2e
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-13 22:26:50 +0300

    Update Role

---
+++ src/main/java/ru/mymsoft/my_jira/model/Role.java
@@ -0,0 +1,31 @@
+package ru.mymsoft.my_jira.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_roles_name", columnNames = {"name"}),
+    })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id", "name"})
+@Builder
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "is_system_role", nullable = false)
+    private Boolean isSystemRole = false;
+}
