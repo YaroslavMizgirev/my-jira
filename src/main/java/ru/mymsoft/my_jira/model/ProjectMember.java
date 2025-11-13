6dcf98b2b8d2acaa83de30069b4fe641dc1fa84b
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-13 22:23:16 +0300

    Update ProjectMember

---
+++ src/main/java/ru/mymsoft/my_jira/model/ProjectMember.java
@@ -0,0 +1,43 @@
+package ru.mymsoft.my_jira.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable; // For composite primary key
+
+@Entity
+@Table(name = "project_members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(ProjectMember.ProjectMemberId.class) // Define composite primary key class
+@Builder
+public class ProjectMember {
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    // Composite primary key class
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    public static class ProjectMemberId implements Serializable {
+        private Long project;
+        private Long group;
+        private Long role;
+    }
+}
