bf2913f99f27ff30658beda4143076da3f7b5de5
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-13 21:58:59 +0300

    Update ActivityLog

--- src/main/java/ru/mymsoft/my_jira/model/ActivityLog.java
+++ src/main/java/ru/mymsoft/my_jira/model/ActivityLog.java
@@ -2,20 +2,19 @@ package ru.mymsoft.my_jira.model;
 
 import java.time.Instant;
 
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "activity_log")
@@ -23,7 +22,8 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = {"id", "actionType"})
+@EqualsAndHashCode(of = {"id"})
+@Builder
 public class ActivityLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
