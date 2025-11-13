f0a9d362baa2812a1399188c68deebb788d55a90
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-13 22:28:13 +0300

    Update RolePermission

---
+++ src/main/java/ru/mymsoft/my_jira/model/RolePermission.java
@@ -0,0 +1,36 @@
+package ru.mymsoft.my_jira.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "role_permissions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(RolePermission.RolePermissionId.class)
+@Builder
+public class RolePermission {
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "permission_id", nullable = false)
+    private Permission permission;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    public static class RolePermissionId implements Serializable {
+        private Long role;
+        private Long permission;
+    }
+}
