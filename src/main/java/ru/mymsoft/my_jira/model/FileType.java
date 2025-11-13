f992c465d83b45d44214557ec3dd3bfb3a047631
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-13 22:00:07 +0300

    Update FileType

---
+++ src/main/java/ru/mymsoft/my_jira/model/FileType.java
@@ -0,0 +1,35 @@
+package ru.mymsoft.my_jira.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.*;
+
+@Entity
+@Table(name = "file_types",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_file_types_extension", columnNames = {"extension"}),
+        @UniqueConstraint(name = "uk_file_types_mime_type", columnNames = {"mime_type"}),
+    })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id", "extension", "mimeType"})
+@Builder
+public class FileType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "extension", nullable = false, length = 20)
+    private String extension;
+
+    @Column(name = "mime_type", nullable = false, length = 100)
+    private String mimeType;
+}
