a827aad02e68912ad2c1828f93ad651d7c4f7eb5
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-14 21:54:33 +0300

    Add ActionType

---
+++ src/main/java/ru/mymsoft/my_jira/model/ActionType.java
@@ -0,0 +1,32 @@
+package ru.mymsoft.my_jira.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Table(name = "action_types")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class ActionType {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", nullable = false, unique = true)
+  @NonNull
+  private String name;
+}
