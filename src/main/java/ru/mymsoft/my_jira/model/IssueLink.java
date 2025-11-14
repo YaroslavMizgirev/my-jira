71ded562d76dcdd2bbf7bab505aae83331cc6d35
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-14 21:56:42 +0300

    Update IssueLink

--- src/main/java/ru/mymsoft/my_jira/model/IssueLink.java
+++ src/main/java/ru/mymsoft/my_jira/model/IssueLink.java
@@ -7,34 +7,50 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.Instant;
 
 @Entity
-@Table(name = "issue_links",
-    uniqueConstraints = {
-        @UniqueConstraint(name = "uk_issue_links_unique_pair1", columnNames = {"source_issue_id", "target_issue_id", "link_type_id"}),
-    })
+@Table(name = "issue_links", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_issue_links_unique_pair1", columnNames = { "source_issue_id", "target_issue_id",
+                "link_type_id" }),
+})
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = {"id"})
+@EqualsAndHashCode(of = { "id" })
 @Builder
 public class IssueLink {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "source_issue_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "source_issue_id", nullable = false, foreignKey = @ForeignKey(
+        name = "fk_issue_links_source_issue",
+        foreignKeyDefinition = "FOREIGN KEY (source_issue_id) REFERENCES public.issue (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE"
+        )
+    )
+    @NonNull
     private Issue sourceIssue;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "target_issue_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "target_issue_id", nullable = false, foreignKey = @ForeignKey(
+        name = "fk_issue_links_target_issue",
+        foreignKeyDefinition = "FOREIGN KEY (target_issue_id) REFERENCES public.issues (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE"
+        )
+    )
+    @NonNull
     private Issue targetIssue;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "link_type_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "link_type_id", nullable = false, foreignKey = @ForeignKey(
+        name = "fk_issue_links_type",
+        foreignKeyDefinition = "FOREIGN KEY (link_type_id) REFERENCES public.issue_link_types (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE RESTRICT"
+        )
+    )
+    @NonNull
     private IssueLinkType linkType;
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
+    @NonNull
     private Instant createdAt;
 }
