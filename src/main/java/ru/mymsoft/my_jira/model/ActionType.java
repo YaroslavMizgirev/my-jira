4ca6797c5b26d5cc4999ce984aaf629228b6d735
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-14 23:51:56 +0300

    Update ActionType

--- src/main/java/ru/mymsoft/my_jira/model/ActionType.java
+++ src/main/java/ru/mymsoft/my_jira/model/ActionType.java
@@ -1,9 +1,9 @@
 package ru.mymsoft.my_jira.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
