e4831fc073da483ec83000a946a22a1891b4f6e0
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2025-11-14 22:02:31 +0300

    Update WorkflowTransition

--- src/main/java/ru/mymsoft/my_jira/model/WorkflowTransition.java
+++ src/main/java/ru/mymsoft/my_jira/model/WorkflowTransition.java
@@ -23,15 +23,30 @@ public class WorkflowTransition {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "from_status_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "from_status_id", nullable = false, foreignKey = @ForeignKey(
+        name = "fk_workflow_transitions_from_status",
+        foreignKeyDefinition = "FOREIGN KEY (from_status_id) REFERENCES public.issue_statuses (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE RESTRICT"
+        )
+    )
+    @NonNull
     private IssueStatus fromStatus;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "to_status_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "to_status_id", nullable = false, foreignKey = @ForeignKey(
+        name = "fk_workflow_transitions_to_status",
+        foreignKeyDefinition = "FOREIGN KEY (to_status_id) REFERENCES public.issue_statuses (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE RESTRICT"
+        )
+    )
+    @NonNull
     private IssueStatus toStatus;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "workflow_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "workflow_id", nullable = false, foreignKey = @ForeignKey(
+        name = "fk_workflow_transitions_workflow",
+        foreignKeyDefinition = "FOREIGN KEY (workflow_id) REFERENCES public.workflows (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE RESTRICT"
+        )
+    )
+    @NonNull
     private Workflow workflow;
 }
