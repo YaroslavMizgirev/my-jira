3244fba8ee179fc039bcbe57d85065831073a456
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2026-01-02 17:44:41 +0300

    Update CreateGroupDto

--- src/main/java/ru/mymsoft/my_jira/dto/CreateGroupDto.java
+++ src/main/java/ru/mymsoft/my_jira/dto/CreateGroupDto.java
@@ -1,14 +1,19 @@
 package ru.mymsoft.my_jira.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "DTO для создания новой группы")
 public record CreateGroupDto(
-    @NotBlank(message = "Name cannot be blank")
+    @NotNull(message = "Name cannot be null")
     @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    @Schema(description = "Название группы", example = "Developers", nullable = false)
     String name,
     
+    @Schema(description = "Описание группы", example = "Group for all developer team members", nullable = true)
     String description,
     
-    @NotBlank(message = "isSystemGroup cannot be blank")
-    boolean isSystemGroup) {}
+    @NotNull(message = "isSystemGroup cannot be null")
+    @Schema(description = "Флаг системной группы", example = "false", nullable = false)
+    Boolean isSystemGroup) {}
