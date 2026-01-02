29475a66af4cf444c7b99fb9ce0b0c8ca87d1dbc
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2026-01-02 17:46:37 +0300

    Update ActionTypeRepository

--- src/main/java/ru/mymsoft/my_jira/repository/ActionTypeRepository.java
+++ src/main/java/ru/mymsoft/my_jira/repository/ActionTypeRepository.java
@@ -4,6 +4,8 @@ package ru.mymsoft.my_jira.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,12 +13,10 @@ import ru.mymsoft.my_jira.model.ActionType;
 
 @Repository
 public interface ActionTypeRepository extends JpaRepository<ActionType, Long> {
-
   Optional<ActionType> findByName(String name);
   boolean existsByName(String name);
 
-  List<ActionType> findByNameContainingIgnoreCase(String namePart);
-  List<ActionType> findByNameStartingWith(String prefix);
+  Page<ActionType> findAllByNameContainingIgnoreCase(String namePart, Pageable pageable);
 
   List<ActionType> findAllByOrderByNameAsc();
   List<ActionType> findAllByOrderByNameDesc();
