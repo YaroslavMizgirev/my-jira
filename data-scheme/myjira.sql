96464bb01dc83d855fec213f23b391681b83f5a7
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2026-01-04 22:34:13 +0300

    Update myjira.sql

--- data-scheme/myjira.sql
+++ data-scheme/myjira.sql
@@ -976,4 +976,16 @@ INSERT INTO public.issue_types (name, icon_url, color_hex_code) VALUES
 ('Improvement', './icons/improvement-50.png', '#16c09eff'),     -- Бирюза для улучшений
 ('Documentation', './icons/documentation-48.png', '#34495E');   -- Тёмно-серый для документации
 
+-- ***********************************************************************************
+--                    INSERT PRIORITIES DEFAULT VALUES
+-- ***********************************************************************************
+
+INSERT INTO public.priorities (level, name, icon_url, color_hex_code) VALUES
+-- Приоритеты от самого высокого к самому низкому
+(1, 'Highest', './icons/priority-highest-48.png', 'rgba(255, 0, 0, 1)'),     -- Красный для наивысшего приоритета
+(2, 'High', './icons/priority-high-48.png', 'rgba(139, 0, 255, 1)'),         -- Фиолетовый для высокого приоритета
+(3, 'Medium', './icons/priority-medium-48.png', 'rgba(255, 157, 0, 1)'),     -- Оранжевый для среднего приоритета
+(4, 'Low', './icons/priority-low-48.png', 'rgba(255, 255, 0, 1)'),           -- Желтый для низкого приоритета
+(5, 'Lowest', './icons/priority-lowest-48.png', 'rgba(0, 0, 255, 1)');       -- Синий для самого низкого приоритета
+
 COMMIT;
