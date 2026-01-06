a432788d2cd38699b5cde5ea73b147a9479a9194
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2026-01-06 12:55:26 +0300

    Update WORKFLOW_STATUSES DEFAULT VALUES

--- data-scheme/workflow-statuses-seed.sql
+++ data-scheme/workflow-statuses-seed.sql
@@ -6,110 +6,47 @@
 
 BEGIN;
 
--- Базовый рабочий процесс - включает все статусы
 INSERT INTO public.workflow_statuses (workflow_id, status_id) VALUES
--- Базовый рабочий процесс (id=1)
-(1, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(1, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(1, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(1, (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
-(1, (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
-(1, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-(1, (SELECT id FROM public.issue_statuses WHERE name = 'On Hold')),
-(1, (SELECT id FROM public.issue_statuses WHERE name = 'Reopened')),
-(1, (SELECT id FROM public.issue_statuses WHERE name = 'Blocked')),
+-- Статусы рабочего процесса для эпиков
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для эпиков'), (SELECT id FROM public.issue_statuses WHERE name = 'Conceived')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для эпиков'), (SELECT id FROM public.issue_statuses WHERE name = 'Research')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для эпиков'), (SELECT id FROM public.issue_statuses WHERE name = 'Approved')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для эпиков'), (SELECT id FROM public.issue_statuses WHERE name = 'Planned')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для эпиков'), (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для эпиков'), (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для эпиков'), (SELECT id FROM public.issue_statuses WHERE name = 'Accepted')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для эпиков'), (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для эпиков'), (SELECT id FROM public.issue_statuses WHERE name = 'On Hold')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для эпиков'), (SELECT id FROM public.issue_statuses WHERE name = 'Rejected')),
 
--- Рабочий процесс для багов (id=2)
-(2, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(2, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(2, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(2, (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
-(2, (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
-(2, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-(2, (SELECT id FROM public.issue_statuses WHERE name = 'On Hold')),
-(2, (SELECT id FROM public.issue_statuses WHERE name = 'Reopened')),
-(2, (SELECT id FROM public.issue_statuses WHERE name = 'Blocked')),
+-- Статусы рабочего процесса для историй
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Research')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Approved')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Planned')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Ready for Development')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'In Developement')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Ready for Code Review')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Code Review Passed')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Code Review Failed')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Ready for QA')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'In QA')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'QA Passed')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'QA Failed')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Ready for Review')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Review Passed')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Review Failed')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'On Hold')),
+((SELECT id FROM public.workflows WHERE name = 'Рабочий процесс для пользовательских историй'), (SELECT id FROM public.issue_statuses WHERE name = 'Rejected'));
 
--- Рабочий процесс для историй (id=3)
-(3, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(3, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(3, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(3, (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
-(3, (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
-(3, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-(3, (SELECT id FROM public.issue_statuses WHERE name = 'On Hold')),
+-- Статусы рабочего процесса для задач
 
--- Рабочий процесс для эпиков (id=4)
-(4, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(4, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(4, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(4, (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
-(4, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-(4, (SELECT id FROM public.issue_statuses WHERE name = 'On Hold')),
+-- Статусы рабочего процесса для багов
 
--- Рабочий процесс для улучшений (id=5)
-(5, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(5, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(5, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(5, (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
-(5, (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
-(5, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-(5, (SELECT id FROM public.issue_statuses WHERE name = 'On Hold')),
+-- Статусы рабочего процесса для улучшений
 
--- Упрощенный рабочий процесс (id=6)
-(6, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(6, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(6, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-
--- Рабочий процесс для документации (id=7)
-(7, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(7, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(7, (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
-(7, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-
--- Расширенный рабочий процесс (id=8)
-(8, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(8, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(8, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(8, (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
-(8, (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
-(8, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-(8, (SELECT id FROM public.issue_statuses WHERE name = 'On Hold')),
-(8, (SELECT id FROM public.issue_statuses WHERE name = 'Reopened')),
-(8, (SELECT id FROM public.issue_statuses WHERE name = 'Blocked')),
-
--- Рабочий процесс с этапом проверки (id=9)
-(9, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(9, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(9, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(9, (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
-(9, (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
-(9, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-(9, (SELECT id FROM public.issue_statuses WHERE name = 'Reopened')),
-
--- Рабочий процесс для Scrum (id=10)
-(10, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(10, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(10, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(10, (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
-(10, (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
-(10, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-
--- Рабочий процесс для Kanban (id=11)
-(11, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(11, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(11, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(11, (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
-(11, (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
-(11, (SELECT id FROM public.issue_statuses WHERE name = 'Closed')),
-(11, (SELECT id FROM public.issue_statuses WHERE name = 'On Hold')),
-
--- Рабочий процесс для Waterfall (id=12)
-(12, (SELECT id FROM public.issue_statuses WHERE name = 'Backlog')),
-(12, (SELECT id FROM public.issue_statuses WHERE name = 'Open')),
-(12, (SELECT id FROM public.issue_statuses WHERE name = 'In Progress')),
-(12, (SELECT id FROM public.issue_statuses WHERE name = 'In Review')),
-(12, (SELECT id FROM public.issue_statuses WHERE name = 'Resolved')),
-(12, (SELECT id FROM public.issue_statuses WHERE name = 'Closed'));
+-- Статусы рабочего процесса для документации
 
 COMMIT;
