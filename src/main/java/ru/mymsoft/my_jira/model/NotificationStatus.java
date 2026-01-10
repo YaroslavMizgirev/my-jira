2ee1d15103e2213c1e5b1e617dd26e9126c7eaa1
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2026-01-10 15:13:38 +0300

    Update NotificationStatus.java

--- src/main/java/ru/mymsoft/my_jira/model/NotificationStatus.java
+++ src/main/java/ru/mymsoft/my_jira/model/NotificationStatus.java
@@ -4,24 +4,26 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "notification_statuses")
-@Getter
-@Setter
+@Table(name = "notification_statuses", uniqueConstraints = {
+    @UniqueConstraint(name = "uk_notification_statuses_name_unique", columnNames = {"name"}),
+})
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder
 public class NotificationStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
     private Long id;
 
     @Column(name = "name", nullable = false, length = 20)
     @NonNull
+    @EqualsAndHashCode.Include
     private String name;
 
-    @Column(name = "is_default", nullable = false)
-    @Builder.Default
-    @NonNull
-    private Boolean isDefault = false;
+    @Column(name = "description", columnDefinition = "TEXT")
+    @EqualsAndHashCode.Exclude
+    private Boolean description = false;
 }
