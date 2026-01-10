70342988b269da046bc96589075ce99bac234a40
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2026-01-10 14:55:54 +0300

    Update ActionType.java

--- src/main/java/ru/mymsoft/my_jira/model/ActionType.java
+++ src/main/java/ru/mymsoft/my_jira/model/ActionType.java
@@ -1,17 +1,10 @@
 package ru.mymsoft.my_jira.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 /**
- * Хранит все типы действий, происходящие с задачами
- * (например, изменение статуса, назначение исполнителя,
- * обновление описания, добавление комментария/вложения).
+ * Хранит все типы действий, происходящие с задачами (например, изменение статуса, назначение исполнителя, обновление описания, добавление комментария/вложения).
  */
 @Entity
 @Table(name = "action_types")
