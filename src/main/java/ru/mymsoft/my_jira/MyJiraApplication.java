46694e45c95805da01fd5647ebe9f5195b774ae1
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2026-01-10 15:13:11 +0300

    Update MyJiraApplication.java

--- src/main/java/ru/mymsoft/my_jira/MyJiraApplication.java
+++ src/main/java/ru/mymsoft/my_jira/MyJiraApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyJiraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyJiraApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(MyJiraApplication.class, args);
+    }
 }
