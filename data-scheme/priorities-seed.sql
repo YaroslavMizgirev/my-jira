e68a27af6c893585eaa0876e7f89979229a4dc9b
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2026-01-10 14:28:42 +0300

    Update PRIORITIES DEFAULT VALUES

--- data-scheme/priorities-seed.sql
+++ data-scheme/priorities-seed.sql
@@ -4,12 +4,12 @@
 
 BEGIN;
 
-INSERT INTO public.priorities (level, name, icon_url, color_hex_code) VALUES
+INSERT INTO public.priorities (level, name, color_hex_code, icon_url) VALUES
 -- Приоритеты от самого высокого к самому низкому
-(1, 'Highest', './icons/priority-highest-48.png', 'rgba(255, 0, 0, 1)'),     -- Красный для наивысшего приоритета
-(2, 'High', './icons/priority-high-48.png', 'rgba(139, 0, 255, 1)'),         -- Фиолетовый для высокого приоритета
-(3, 'Medium', './icons/priority-medium-48.png', 'rgba(255, 157, 0, 1)'),     -- Оранжевый для среднего приоритета
-(4, 'Low', './icons/priority-low-48.png', 'rgba(255, 255, 0, 1)'),           -- Желтый для низкого приоритета
-(5, 'Lowest', './icons/priority-lowest-48.png', 'rgba(0, 0, 255, 1)');       -- Синий для самого низкого приоритета
+(1, 'Highest', 'rgba(255, 0, 0, 1)', './icons/priority-highest-48.png'), -- Красный для наивысшего приоритета
+(2, 'High', 'rgba(139, 0, 255, 1)', './icons/priority-high-48.png'),     -- Фиолетовый для высокого приоритета
+(3, 'Medium', 'rgba(255, 157, 0, 1)', './icons/priority-medium-48.png'), -- Оранжевый для среднего приоритета
+(4, 'Low', 'rgba(255, 255, 0, 1)', './icons/priority-low-48.png'),       -- Желтый для низкого приоритета
+(5, 'Lowest', 'rgba(0, 0, 255, 1)', './icons/priority-lowest-48.png'); -- Синий для самого низкого приоритета
 
 COMMIT;
