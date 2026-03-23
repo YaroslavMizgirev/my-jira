e871f6811cca482919df622232dd6b1e1e493762
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2026-03-23 14:06:44 +0300

    Update 22.03.2026

--- src/main/java/ru/mymsoft/my_jira/dto/IssueTypeDto.java
+++
@@ -1,7 +0,0 @@
-package ru.mymsoft.my_jira.dto;
-
-public record IssueTypeDto (
-    Long id,
-    String name,
-    String iconUrl,
-    String colorHexCode) {}
--- src/main/resources/db/migration/V1__Baseline.sql
+++
@@ -1,5 +0,0 @@
---
--- Baseline migration for existing schema
--- Schema is already created by Docker initialization
---
-SELECT 1; -- Пустая миграция для установки базовой линии
